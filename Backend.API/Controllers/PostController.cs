1ea3f00f630e9cac69389e87ff2d97ea2713958d
Angel Humberto Castorena Lugo <ahcl@thinkcares.com>
2021-01-21 20:31:16 -0700

    first commit

---
+++ Backend.API/Controllers/PostController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Backend.API.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetPosts()
+        {
+
+            return Ok(null);
+        }
+    }
+}
---
+++ Backend.Domain/Entities/Post.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Backend.Domain.Entities
+{
+    public class Post
+    {
+        public object MyProperty { get; set; }
+    }
+}
